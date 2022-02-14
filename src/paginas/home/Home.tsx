611cf879551588fadb57afe381bb7c886c0fe628
Cibele França <cibeletessinha@gmail.com>
2022-02-14 20:01:59 -0300

    Criação Home em React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <div>
+            <h1 className="Titulo"> Home </h1>
+            <img src="https://www.santosfc.com.br/wp-content/uploads/2021/07/Arte-Comunicado-Site-2021.png" alt="Imagem Inicial" width="500" height= "500"/>
+        </div>
+    );
+}
+export default Home;
