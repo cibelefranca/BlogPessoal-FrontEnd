5ab0c683a4af14f30a280b7b82de6775b74b8864
Cibele França <cibeletessinha@gmail.com>
2022-02-23 21:44:56 -0300

    Atualizações

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Box, Typography, Button } from "@material-ui/core";
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import './Home.css';
 
 function Home(){
@@ -26,6 +27,7 @@ function Home(){
                 </Grid>
 
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
 
             </Grid>
